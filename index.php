93ea1f362c6fcf389524e778c72a30195734719d
mchbtbs <mailmicahtobias@gmail.com>
2020-10-21 18:13:02 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ * Main template file
+ * 
+ * @package wptheme2020
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello WordPress!</h1>
+</body>
+</html>
